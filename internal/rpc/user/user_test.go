89450e76bd36818ff4f70857d47101a7c2508013
truongpx-Mac <truongpx396@gmail.com>
2024-10-08 12:47:13 +0700

    update

--- internal/rpc/user/user_test.go
+++ internal/rpc/user/user_test.go
@@ -31,7 +31,7 @@ func TestGetDesignateUsers(t *testing.T) {
 	res, err := uServer.GetDesignateUsers(ctx, &pbuser.GetDesignateUsersReq{UserIDs: []string{}})
 
 	assert.NoError(t, err)
-	assert.Equal(t, "99", res.UsersInfo[0].UserID)
+	assert.Equal(t, "991", res.UsersInfo[0].UserID)
 
 	t.Logf("success")
 }
